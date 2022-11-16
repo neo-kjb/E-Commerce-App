5ef767e24f9c0f07801caac5b4c280ab0cc5d06b
neo-kjb <anaszawahreh.dev@gmail.com>
2022-11-16 22:06:55 +0300

    'New Product' Validation

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -2,8 +2,15 @@ const { check } = require('express-validator')
 const usersRepo = require('../../Repositories/users')
 
 module.exports = {
-  requireTitle: check('title').trim().isLength({ min: 1, max: 20 }),
-  requirePrice: check('price').trim().toFloat().isFloat({ min: 1 }),
+  requireTitle: check('title')
+    .trim()
+    .isLength({ min: 1, max: 40 })
+    .withMessage('Must be between 1 and 40 characters'),
+  requirePrice: check('price')
+    .trim()
+    .toFloat()
+    .isFloat({ min: 1 })
+    .withMessage('Must be a number greater than 1'),
   requireEmail: check('email')
     .trim()
     .normalizeEmail()
