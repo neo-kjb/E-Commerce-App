df03d818a234b20ea1f1a0eeff0cf571cd46b111
neo-kjb <anaszawahreh.dev@gmail.com>
2022-11-16 21:55:33 +0300

    'New Product' Validation

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const { validationResult } = require('express-validator')
 const productsRepo = require('../../Repositories/products')
 const productsNewTemplate = require('../../views/admin/products/new')
 const router = express.Router()
+const { requireTitle, requirePrice } = require('./validators')
 
 router.get('/admin/products', (req, res) => {})
 
 router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}))
+})
+
+router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
+  const errors = validationResult(req)
+  console.log(errors)
+  res.send('submitted')
 })
 
 module.exports = router
